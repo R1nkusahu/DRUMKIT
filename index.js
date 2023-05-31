1408755500b1b37fac2838c033077ff2318bae47
R1nkusahu <91043232+R1nkusahu@users.noreply.github.com>
2023-05-31 17:27:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,22 +41,22 @@ function makeSound(key) {
       tom3.play();
       break;
 
-    case "s":
+    case "p":
       var tom4 = new Audio('tom-4.mp3');
       tom4.play();
       break;
 
-    case "t":
+    case "s":
       var snare = new Audio('snare.mp3');
       snare.play();
       break;
 
-    case "h":
+    case "l":
       var crash = new Audio('crash.mp3');
       crash.play();
       break;
 
-    case "a":
+    case "m":
       var kick = new Audio('kick-bass.mp3');
       kick.play();
       break;
